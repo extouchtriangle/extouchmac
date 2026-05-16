5203dd16f0f8b59d91a145c31ce1b69721d485d6
Easton Wei <eastonwei12@gmail.com>
2026-05-15 21:33:25 -0400

    AAAA

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
-modulemodule.exports = {
+module.exports = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
