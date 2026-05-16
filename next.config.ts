77950e74cbd894821341b863e59903dbaab84fff
Easton Wei <eastonwei12@gmail.com>
2026-05-15 21:31:47 -0400

    ignore compleetly

--- next.config.ts
+++ next.config.ts
@@ -6,3 +6,10 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+modulemodule.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
